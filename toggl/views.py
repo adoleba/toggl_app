d717a0ca71b0b04e0ff2c68455a5422338a5e0d5
adoleba <ania.doleba@wp.pl>
2020-02-10 12:55:24 +0100

    save toogle entry if fixed working hours

--- toggl/views.py
+++ toggl/views.py
@@ -1,8 +1,17 @@
 from datetime import timedelta
 
-from django.http import HttpResponseRedirect, HttpResponse
+import requests
+from django.http import HttpResponse
 from django.shortcuts import render
+
 from toggl.forms import EntryForm
+
+headers = {
+    "Authorization": "",
+    "Content-Type": "application/json",
+    "Accept": "*/*",
+    "User-Agent": "python/urllib",
+}
 
 
 def index(request):
@@ -15,12 +24,21 @@ def index(request):
 
             date_start = form.cleaned_data['date_start']
             date_end = form.cleaned_data['date_end']
+            hour_start = form.cleaned_data['hour_start']
+            hour_end = form.cleaned_data['hour_end']
+
+            task = form.cleaned_data['task']
+            toggl_login = form.cleaned_data['toggl_login']
+            toggl_id_number = form.cleaned_data['toggl_id_number']
+            toggl_password = form.cleaned_data['toggl_password']
 
             week_days['first_monday'] = date_start + timedelta(days=0 - date_start.weekday())
             week_days['first_tuesday'] = date_start + timedelta(days=1 - date_start.weekday())
             week_days['first_wednesday'] = date_start + timedelta(days=2 - date_start.weekday())
             week_days['first_thursday'] = date_start + timedelta(days=3 - date_start.weekday())
             week_days['first_friday'] = date_start + timedelta(days=4 - date_start.weekday())
+
+            duration_in_sec = (hour_end.hour - hour_start.hour) * 3600
 
             for day in week_days:
                 date = week_days[day]
@@ -31,7 +49,26 @@ def index(request):
                     working_days.append(next_day)
                     next_day += timedelta(days=7)
 
-            return HttpResponseRedirect('/')
+            for day in working_days:
+                data = {
+                    "time_entry": {
+                        "description": task,
+                        "duration": str(duration_in_sec),
+                        "start": str('{:04d}'.format(day.year)) + "-" + str('{:02d}'.format(day.month)) + "-" + str(
+                            '{:02d}'.format(day.day)) + "T" + str('{:02d}'.format(hour_start.hour - 1)) + ":" + str(
+                            '{:02d}'.format(hour_start.minute)) + ":00.000Z",
+                        "id": toggl_id_number,
+                        "created_with": "curl",
+                    }
+                }
+                resp = requests.post(
+                    'https://www.toggl.com/api/v8/time_entries',
+                    auth=(toggl_login, toggl_password),
+                    json=data,
+                    headers=headers,
+                )
+
+            return HttpResponse('Dodano')
 
     else:
         form = EntryForm(request.POST)
