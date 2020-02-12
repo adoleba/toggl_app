2efdf3b1ab44f63ac1f4fba62fcf311138142a83
adoleba <ania.doleba@wp.pl>
2020-02-12 13:57:44 +0100

    js function for choosing hours (regular or variable)

---
+++ static/js/custom.js
@@ -0,0 +1,9 @@
+function regularHours() {
+    document.getElementById('regular-hours').style.display = 'block';
+    document.getElementById('variable-hours').style.display = 'none'
+}
+
+function variableHours() {
+    document.getElementById('variable-hours').style.display = 'block';
+    document.getElementById('regular-hours').style.display = 'none';
+}
