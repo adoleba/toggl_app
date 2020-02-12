a68dfcd415fe6be87e4a9272943e89f4ce8b14ac
adoleba <ania.doleba@wp.pl>
2020-02-12 14:55:32 +0100

    initial data for fields date_start, date_end, hour_start, hour_end

--- toggl/forms.py
+++ toggl/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from toggl.initial_data import start_day, end_day
+
 
 class DateInput(forms.DateInput):
     input_type = 'date'
@@ -15,13 +17,13 @@ class PasswordInput(forms.PasswordInput):
 
 class EntryForm(forms.Form):
     task = forms.CharField(label='Zadanie', max_length=50)
-    date_start = forms.DateField(label='Początek zadania', widget=DateInput)
-    date_end = forms.DateField(label='Koniec zadania', widget=DateInput)
+    date_start = forms.DateField(label='Początek zadania', widget=DateInput, initial=start_day)
+    date_end = forms.DateField(label='Koniec zadania', widget=DateInput, initial=end_day)
     toggl_login = forms.CharField(label='Login do konta Toggl', max_length=50)
     toggl_id_number = forms.IntegerField(label='Numer id konta Toggl')
     toggl_password = forms.CharField(label='Hasło do konta Toggl', max_length=50, widget=PasswordInput)
-    hour_start = forms.TimeField(label='Godzina rozpoczęcia', widget=TimeInput, required=False)
-    hour_end = forms.TimeField(label='Godzina zakończenia', widget=TimeInput, required=False)
+    hour_start = forms.TimeField(label='Godzina rozpoczęcia', widget=TimeInput, required=False, initial="10:00:00.00")
+    hour_end = forms.TimeField(label='Godzina zakończenia', widget=TimeInput, required=False, initial="18:00:00.00")
     monday_hour_start = forms.TimeField(widget=TimeInput, required=False)
     monday_hour_end = forms.TimeField(widget=TimeInput, required=False)
     tuesday_hour_start = forms.TimeField(widget=TimeInput, required=False)
