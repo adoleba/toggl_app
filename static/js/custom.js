af0c59db67cde2f8988d4349ba69d7ab1306f516
adoleba <ania.doleba@wp.pl>
2020-02-13 13:13:18 +0100

    js and style for variable hours

--- static/js/custom.js
+++ static/js/custom.js
@@ -7,3 +7,17 @@ function variableHours() {
     document.getElementById('variable-hours').style.display = 'block';
     document.getElementById('regular-hours').style.display = 'none';
 }
+
+function showDay(id, add) {
+    const dayId = document.getElementById(id);
+    const addDayId = document.getElementById(add);
+    dayId.style.display = 'block';
+    addDayId.style.display = 'none';
+}
+
+function hideDay(id, add) {
+    const dayId = document.getElementById(id);
+    const addDayId = document.getElementById(add);
+    dayId.style.display = 'none';
+    addDayId.style.display = 'block';
+}
