35c27fc8461b17bc5321eab7329dcff82ce61e49
adoleba <ania.doleba@wp.pl>
2020-02-18 11:50:54 +0100

    function reloadHours - shows hours checked before validation error  and mark unfilled hours as red field

--- static/js/custom.js
+++ static/js/custom.js
@@ -22,3 +22,44 @@ function hideDay(id, add) {
     dayId.style.display = 'none';
     addDayId.style.display = 'block';
 }
+
+function reloadHours() {
+    if (document.getElementById('id_different_hours_0').checked === true) {
+        document.getElementById('regular-hours').style.display = 'block';
+        document.getElementById('variable-hours').style.display = 'none'
+    }
+    if (document.getElementById('id_different_hours_1').checked === true) {
+        document.getElementById('variable-hours').style.display = 'block';
+        document.getElementById('regular-hours').style.display = 'none';
+    }
+
+    if (document.getElementById('id_monday_hour_start').value.length !== 0 ||
+        document.getElementById('id_monday_hour_end').value.length !== 0) {
+        document.getElementById('monday-hours').style.display = 'block';
+        document.getElementById('monday-add').style.display = 'none';
+    }
+
+    if (document.getElementById('id_tuesday_hour_start').value.length !== 0 ||
+        document.getElementById('id_tuesday_hour_end').value.length !== 0) {
+        document.getElementById('tuesday-hours').style.display = 'block';
+        document.getElementById('tuesday-add').style.display = 'none';
+    }
+
+    if (document.getElementById('id_wednesday_hour_start').value.length !== 0 ||
+        document.getElementById('id_wednesday_hour_end').value.length !== 0) {
+        document.getElementById('wednesday-hours').style.display = 'block';
+        document.getElementById('wednesday-add').style.display = 'none';
+    }
+
+    if (document.getElementById('id_thursday_hour_start').value.length !== 0 ||
+        document.getElementById('id_thursday_hour_end').value.length !== 0) {
+        document.getElementById('thursday-hours').style.display = 'block';
+        document.getElementById('thursday-add').style.display = 'none';
+    }
+
+    if (document.getElementById('id_friday_hour_start').value.length !== 0 ||
+        document.getElementById('id_friday_hour_end').value.length !== 0) {
+        document.getElementById('friday-hours').style.display = 'block';
+        document.getElementById('friday-add').style.display = 'none';
+    }
+}
