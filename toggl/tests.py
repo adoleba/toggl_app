6bb021dddf80ab2eea14f266b9d60560c2e6df23
adoleba <ania.doleba@wp.pl>
2020-02-26 15:09:46 +0100

    MainPageTest for get and post, using Client

--- toggl/tests.py
+++ toggl/tests.py
@@ -1,8 +1,15 @@
 from django.http import HttpRequest
 from django.test import TestCase, Client
 from django.urls import resolve
+from django.utils.html import escape
 
+from toggl.forms import EntryForm
 from toggl.views import done
+
+
+correct_toggl_dict = {'task': 'test', 'date_start': '2020-02-01', 'date_end': '2020-02-29', 'different_hours': 'R',
+                   'hour_start': '10:00', 'hour_end': '18:00', 'toggl_login': 'edfrgthyuj@wp.pl',
+                   'toggl_password': 'swdefr', 'toggl_id_number': '2345'}
 
 
 class DonePageTest(TestCase):
@@ -34,3 +41,36 @@ class DonePageTestWithClient(TestCase):
 
     def test_template_is_not_empty(self):
         self.assertNotEqual(self.response.context, "None")
+
+
+class MainPageTestGet(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.response = self.client.get('/')
+
+    def test_used_template_is_correct(self):
+        self.assertTemplateUsed(self.response, 'toggl/index.html')
+
+    def test_status_code_is_200(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_template_is_not_empty(self):
+        self.assertNotEqual(self.response.context, "None")
+
+    def test_form_in_context(self):
+        self.assertIsInstance(self.response.context['form'], EntryForm)
+
+
+class MainPageTestPost(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_task_error_if_form_empty(self):
+        response = self.client.post('/')
+        expected_error = escape('Wpisz nazwę zadania')
+        self.assertContains(response, expected_error)
+
+    # long time
+    def test_redirect_after_correct_post(self):
+        response = self.client.post('/', data=correct_toggl_dict)
+        self.assertRedirects(response, '/done/')
