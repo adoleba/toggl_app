cb7c53664f795a2964f791b053896dec37a886b5
adoleba <ania.doleba@wp.pl>
2020-02-26 16:10:48 +0100

    EntryViewTest

--- toggl/tests.py
+++ toggl/tests.py
@@ -1,15 +1,15 @@
 from django.http import HttpRequest
-from django.test import TestCase, Client
+from django.test import TestCase, Client, RequestFactory
 from django.urls import resolve
 from django.utils.html import escape
 
 from toggl.forms import EntryForm
-from toggl.views import done
-
+from toggl.initial_data import start_day, end_day
+from toggl.views import done, EntryView
 
 correct_toggl_dict = {'task': 'test', 'date_start': '2020-02-01', 'date_end': '2020-02-29', 'different_hours': 'R',
-                   'hour_start': '10:00', 'hour_end': '18:00', 'toggl_login': 'edfrgthyuj@wp.pl',
-                   'toggl_password': 'swdefr', 'toggl_id_number': '2345'}
+                      'hour_start': '10:00', 'hour_end': '18:00', 'toggl_login': 'edfrgthyuj@wp.pl',
+                      'toggl_password': 'swdefr', 'toggl_id_number': '2345'}
 
 
 class DonePageTest(TestCase):
@@ -74,3 +74,42 @@ class MainPageTestPost(TestCase):
     def test_redirect_after_correct_post(self):
         response = self.client.post('/', data=correct_toggl_dict)
         self.assertRedirects(response, '/done/')
+
+
+class EntryViewTest(TestCase):
+    def setUp(self):
+        self.request = RequestFactory().get('/')
+        self.form = EntryForm()
+
+    def test_view_in_template_context(self):
+        view = EntryView()
+        view.setup(self.request)
+        context = view.get_context_data()
+        self.assertIn('view', context)
+
+    def test_render_input_form_in_template(self):
+        view = EntryView.as_view()
+        response = view(self.request)
+        content = response.rendered_content
+        self.assertIn('input', content)
+
+    def test_render_buttons_for_hours_choice(self):
+        view = EntryView.as_view()
+        response = view(self.request)
+        content = response.rendered_content
+        self.assertIn('btn-group', content)
+
+    def test_render_send_button_form_in_template(self):
+        view = EntryView.as_view()
+        response = view(self.request)
+        content = response.rendered_content
+        self.assertIn('button', content)
+
+    def test_form_has_initial_start_day(self):
+        self.assertEqual(self.form['date_start'].initial, start_day())
+
+    def test_form_has_initial_end_day(self):
+        self.assertEqual(self.form['date_end'].initial, end_day())
+
+    def test_task_field_form_has_class_from_widget(self):
+        self.assertIn('class="form-control"', self.form['task'].as_widget())
